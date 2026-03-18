cd326e5caf5d86b9db44659c4ea7e0dfe4fa9fe1
Lautaro Hollmann <l.hollmann.lh@gmail.com>
2026-03-18 17:00:01 +0100

    overhauls getComputerChoice function logic/syntax

--- javascript.js
+++ javascript.js
@@ -8,21 +8,39 @@ console.log("Hello World");
 
 // Create a function for computor to randomly choose one of the three options
 
-function getComputerChoice() {
-    if (Math.floor(Math.random() <= 0.33333333333)) {
+// Like this it's from 0-1 (I divide by 3 for the three options. NOT SO GOOD):
+
+/*function getComputerChoice() {
+  let random = Math.random();
+    if (random <= 0.33333333333) {
         return "Rock"
-    } else if (Math.floor(Math.random() > 0.33333333333 && Math.random() < 0.66666666666)) {
+    } else if (random > 0.33333333333 && random < 0.66666666666) {
         return "Paper"
        } else {
         return "Scissors"
     }
+}*/
+
+
+//This way is better, as we extend the range to 1-3 (instead of 0-1):
+
+function getComputerChoice() {
+  let option = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+  /*function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+  +1 in parenthesis after "max - min" for the maximum and minimun to be inclusive*/
+
+  // Declares a variable called: "option", which consists of a randomly generated number selected from: 1, 2 or 3.
+
+   if (option === 1) {
+    return "Rock";
+  } else if (option === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
-/*not quite sure if this works as intended so far
 
-This way would probably be better/correct:
-function getComputerChoice(min, max) {
-  return Math.random() * (max - min) + 1;
-}
-*/
 
